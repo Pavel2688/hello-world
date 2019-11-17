4feb57f4b8c4b48cb6ae0be726ad18a8ec46aeee
Pavel Ogorodnikov <ogorodnikovpavel807@gmail.com>
2019-11-17 18:49:18 +0400

    hello world 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+        int n = 10;
         String hello = "Hello";
         String world = "World!";
 	    System.out.println(hello + " " + world);
